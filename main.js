b9cf754a4b3704d95640ecca089d4410d6204f21
Martin Alvarez <tinchoa02@gmail.com>
2022-05-16 12:26:49 -0300

    Proyecto php

--- main.js
+++ main.js
@@ -31,7 +31,24 @@ sr.reveal ('.contact-text',{delay:200})
 sr.reveal ('.contact-icons',{delay:300})
 
 
+
 //Ventana Pop Up
+
+// Seleccionamos todas las cards
+const cards = document.querySelectorAll('.card')
+
+// Añadimos un evento en cada click
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        if (card.classList.contains("active")) {
+            card.classList.toggle("active");
+        } else {
+            cards.forEach((card) => card.classList.remove("active"));
+            card.classList.add("active");
+        }
+
+    })
+})
 
 
 
